e4a7aab515beff998615b2ad5677bc1faa347cb3
Norton M. B. Santos <norton@alunos.utfpr.edu.br>
2018-09-25 13:00:43 -0300

    organizing folders and add Viacep

--- view/sidebar.php
+++ view/sidebar.php
@@ -1,5 +1,5 @@
 <ul id="slide-out" class="sidenav sidenav-fixed">
   <li><a class="red" href="#!"></a></li>
-  <li><a href="/tratador-endereco-php/view/new.php">Cadastrar</a></li>
+  <li><a href="/tratador-endereco-php/view/usuario/new.php">Cadastrar</a></li>
   <li><a href="#!">Listar</a></li>
 </ul>
--- view/usuario/form.php
+++ view/usuario/form.php
@@ -6,6 +6,10 @@
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+            crossorigin="anonymous"></script>
+    <script src="/tratador-endereco-php/public/js/cep.js"></script>
      
 </head>
 <body>
@@ -22,11 +26,12 @@
 <div class="center">
   <form class="col s6">
     <div class="row">
-      <input placeholder="Name" name="name" type="text" class="validate">
-      <input placeholder="CEP" name="cep" type="text" class="validate">
-      <input placeholder="Estado" name="state" type="text" class="validate">
-      <input placeholder="Cidade" name="city" type="text" class="validate">
-      <input placeholder="Rua" name="street" type="text" class="validate">
+      <input placeholder="Nome" name="name" type="text" class="validate">
+      <input placeholder="CEP" id="cep" name="cep" type="text" class="validate">
+      <input placeholder="Estado" id="uf" name="state" type="text" class="validate">
+      <input placeholder="Cidade" id="cidade" name="cidade" type="text" class="validate">
+      <input placeholder="Bairro" id="bairro" name="district" type="text" class="validate">
+      <input placeholder="Rua" id="rua" name="street" type="text" class="validate">
       <input placeholder="Número" name="number" type="text" class="validate">
       <input placeholder="Complemento (Bloco, apartamento, etc.)" name="complement" type="text" class="validate">
       <button type="submit" class="btn green right">Salvar</button>
